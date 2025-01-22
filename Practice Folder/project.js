3964e147974fc10c7c5fa80b931002eae8562ba7
Wsanchez91 <williamsanchez.8055@gmail.com>
2025-01-22 13:58:26 -0500

    update

--- Practice Folder/project.js
+++ Practice Folder/project.js
@@ -13,5 +13,20 @@ const deposit = () => {
   }
 };
 
-const depositAmount = deposit();
-console.log(depositAmount);
+const getNumberOfLines = () => {
+  while (true){
+    const lines = prompt("Enter the number of lines to bet on (1-3):");
+    const numberOfLines = parseFloat(lines);
+
+    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+      console.log('Invalid number of lines, try again');
+    } else {
+      return numberOfLines;
+    }
+  }
+};
+
+const numberDepositAmount = deposit();
+const numberOfLines = getNumberOfLines();
+console.log(numberDepositAmount);
+console.log(numberOfLines);
